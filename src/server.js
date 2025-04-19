8f6b15b5f371549919f57c76671c7b3fb22eec60
mvdl <contatomvdl@hotmail.com>
2025-04-19 00:02:22 -0300

    basic server http

--- src/server.js
+++ src/server.js
@@ -1,8 +1,33 @@
 const http = require("http");
 
+const PORT = 8000;
+
 http
   .createServer((req, res) => {
-    res.write("hello world");
-    res.end();
+    const { url } = req;
+
+    switch (url) {
+      case "/users":
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Users");
+        break;
+
+      case "/products":
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Products");
+        break;
+
+      case "/health":
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("OK");
+        break;
+
+      default:
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found");
+        break;
+    }
   })
-  .listen(8000);
+  .listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
