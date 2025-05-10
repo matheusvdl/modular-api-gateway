b79bcbcdfc74c949d9281e7a579a54182a10b2b8
mvdl <contatomvdl@hotmail.com>
2025-05-10 17:56:05 -0300

    intercept user route just for test

--- src/server.js
+++ src/server.js
@@ -1,6 +1,38 @@
 const http = require("http");
+const https = require("https");
 
 const PORT = 8000;
+
+const interceptUserRoute = (clientRes) => {
+  const options = {
+    hostname: "jsonplaceholder.typicode.com",
+    path: "/users",
+    method: "GET",
+  };
+
+  const proxyReq = https.request(options, (proxyRes) => {
+    let data = "";
+
+    proxyRes.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    proxyRes.on("end", () => {
+      clientRes.writeHead(proxyRes.statusCode, {
+        "Content-Type": proxyRes.headers["content-type"] || "application/json",
+      });
+      clientRes.end(data);
+    });
+  });
+
+  proxyReq.on("error", (e) => {
+    clientRes.writeHead(502, { "Content-Type": "text/plain" });
+    clientRes.end("Bad Gateway");
+    console.error("Proxy error:", e.message);
+  });
+
+  proxyReq.end();
+};
 
 http
   .createServer((req, res) => {
@@ -8,8 +40,7 @@ http
 
     switch (url) {
       case "/users":
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("Users");
+        interceptUserRoute(res);
         break;
 
       case "/products":
